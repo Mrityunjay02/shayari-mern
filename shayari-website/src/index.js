e743e293c198e89001fb34f73a1d7e043ba31a5c
Mrityunjay02 <mjdarkey02@gmail.com>
2025-02-26 13:12:25 +0530

    fix: update fontawesome import and package version

--- shayari-website/src/index.js
+++ shayari-website/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import '@fortawesome/fontawesome-free/css/all.css';  
+import '@fortawesome/fontawesome-free/css/all.min.css';  // Using the minified version
 import App from './App';
 import './index.css'; // Import global styles if needed
 
