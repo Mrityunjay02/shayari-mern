7ae57cdcc2bf9a983a191d92e7f8c3c561b45893
Mrityunjay02 <mjdarkey02@gmail.com>
2025-02-26 13:15:52 +0530

    fix: remove FontAwesome CSS import

--- shayari-website/src/index.js
+++ shayari-website/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import '@fortawesome/fontawesome-free/css/all.min.css';  // Using the minified version
+// Remove FontAwesome CSS import and use components directly
 import App from './App';
 import './index.css'; // Import global styles if needed
 
